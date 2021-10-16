a854eb44e2d217a19fee34b9383e1c50a74b41e8
Tej Sahu <tjsahu@gmail.com>
2021-10-16 08:35:16 +0530

    commit changes to api code

--- api.py
+++ api.py
@@ -1,20 +1,26 @@
-from flask import Flask, request
+from flask import Flask
 import json
 
 app = Flask(__name__)
 
 # Best Need for Speed Games
 games = [{'id': 0,
-            'title': 'Need for Speed Heat',
-            'year_published': '2019'},
-        {'id': 1,
-            'title': 'Need for Speed Most Wanted',
-            'published': '2005'},
-        {'id': 2,
-            'title': 'Need for Speed Underground 2',
-            'published': '2004'}]
+          'title': 'Need for Speed Heat',
+          'year_published': '2019'},
+         {'id': 1,
+          'title': 'Need for Speed Most Wanted',
+          'published': '2005'},
+         {'id': 2,
+          'title': 'Need for Speed Underground 2',
+          'published': '2004'},
+         {'id': 3,
+          'title': 'Need for Speed Prostreet',
+          'published': '2012'}]
+
 
 games_json = json.dumps(games)
+
+
 @app.route('/')
 def home():
     return '''<h1>Best Need for Speed titles</h1><p>API Sample returning best NFS Games</p>'''
@@ -24,6 +30,7 @@ def home():
 def games():
     return games_json
 
-#app.run(host= '0.0.0.0')
+
+# app.run(host= '0.0.0.0')
 app.run()
 
